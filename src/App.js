57af547024421ed81e29cb7751765b20aa4b7690
Diego <diego2003coila@gmail.com>
2023-11-09 09:46:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           <code> 09/11/2023 - TURNO MAÑANA</code> 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          SALUDOS <br></br>
+          ALVARADO DIAZ BENJAMIN PAOLO
         </a>
       </header>
     </div>
